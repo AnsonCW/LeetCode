954f5907fab40c8c2e76aa9adcc7486f9377cc6d
Anson <gn01201070@gmail.com>
2018-01-18 22:38:22 +0800

    [Easy] Hamming Distance

---
+++ src/Easy/HammingDistance.java
@@ -0,0 +1,32 @@
+package Easy;
+
+/**
+ * The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+ * Given two integers x and y, calculate the Hamming distance.
+ * 
+ * Note:
+ * 0 ≤ x, y < 2^31.
+ * 
+ * Example:
+ * Input: x = 1, y = 4
+ * Output: 2
+ * Explanation:
+ * 1   (0 0 0 1)
+ * 4   (0 1 0 0)
+ *        ↑           ↑
+ * The above arrows point to positions where the corresponding bits are different.
+ * 
+ * 計算兩數的漢明距離
+ * https://zh.wikipedia.org/wiki/漢明距離
+ *
+ */
+public class HammingDistance {
+
+	public static void main(String[] args) {
+		System.out.println(hammingDistance(1,4));
+	}
+	
+	public static int hammingDistance(int x, int y) {
+        return Integer.bitCount( x ^ y );
+    }
+}
