7701e456f7cd654179e5563f283416478ca1a2b8
Anson <gn01201070@gmail.com>
2018-01-18 21:23:10 +0800

    [Easy] Move Zeroes

--- src/Easy/MoveZeroes.java
+++ src/Easy/MoveZeroes.java
@@ -1,5 +1,7 @@
 package Easy;
 
+import java.util.Arrays;
+
 /**
  * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
  * For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
@@ -15,24 +17,11 @@ package Easy;
 public class MoveZeroes {
 	
 	public static void main(String[] args) {
-		int nums[] = new int[10];
-		nums[0] = 4;  
-		nums[1] = 2;   
-		nums[2] = 4;   
-		nums[3] = 0;  
-		nums[4] = 0; 
-		nums[5] = 3;
-		nums[6] = 0;
-		nums[7] = 5;
-		nums[8] = 1;
-		nums[9] = 0;
+		int nums[] = {4,2,4,0,0,3,0,5,1,0};
 		
-		moveZeroes(nums) ;
+		moveZeroes(nums);
 		
-		for( Integer i : nums )
-		{
-			System.out.println(i);
-		}
+		System.out.println(Arrays.toString(nums));
 	}
 	
 	public static void moveZeroes(int[] nums) {
@@ -53,7 +42,6 @@ public class MoveZeroes {
 			
 			x = 1;
 		}
-		
 		
     }
 }
